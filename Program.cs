fab0909be41d6b8f9af8cc3eb11703d59ff173db
Aleksei Korol <joker.enot@gmail.com>
2024-03-05 21:22:23 +1000

    финальный экземпляр кода

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿string[] initial_array = {"dfghjkjhgfcx","g","tt","gglkkm"};
 
+string[] SortOutAnArray(string[] user_input) {
+    string final_str = string.Empty;
+    for (int i = 0; i < user_input.Length; i++){
+        if(user_input[i].Length < 4){
+            final_str += user_input[i];
+            final_str += " ";
+        }
+    }
+    Console.Write(final_str);
+    return final_str.Split(' ');
+}
+
 
 if (initial_array.Length == 1 && initial_array[0] == "-") {
     Console.WriteLine("Write down an array with , in between words");
     string user_input = Console.ReadLine();
     string[] to_be_modified = user_input.Split(',');
-    //func to and check arr
+    SortOutAnArray(to_be_modified);
 } else {
-    //func to and check arr
+    SortOutAnArray(initial_array);
 }
