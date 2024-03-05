7f3adf5a887e57d8e8d0ca93172c9eac02e01c36
Aleksei Korol <joker.enot@gmail.com>
2024-03-05 21:20:40 +1000

    created a if statement to check if the arr is empty to ask user for input

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿string[] initial_array = {"dfghjkjhgfcx","g","tt","gglkkm"};
+
+
+if (initial_array.Length == 1 && initial_array[0] == "-") {
+    Console.WriteLine("Write down an array with , in between words");
+    string user_input = Console.ReadLine();
+    string[] to_be_modified = user_input.Split(',');
+    //func to and check arr
+} else {
+    //func to and check arr
+}
